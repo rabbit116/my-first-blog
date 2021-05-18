816412844d8f19da9ddc4d9b6ec595c32166da2b
murata <murata.test@example.com>
2021-05-18 16:04:52 +0900

    DIR

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR, "blog/post_list.html"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
